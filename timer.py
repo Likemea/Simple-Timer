dc1be1e37c09a8efbaf39c3edb78481bae5b44c6
Likemea <106814227+Likemea@users.noreply.github.com>
2023-10-06 20:14:14 +0200

    expanded functionality
    now works as its own window .

--- timer.py
+++ timer.py
@@ -1,14 +1,24 @@
 import tkinter as tk
 
 def start_timer():
-
+    global seconds_left
     minutes = int(entry.get())
-    seconds = minutes * 60
-    
+    seconds_left = minutes * 60
+    update_timer()
+
+def update_timer():
+    global seconds_left
+    if seconds_left > 0:
+        mins, secs = divmod(seconds_left, 60)
+        timeformat = '{:02d}:{:02d}'.format(mins, secs)
+        timer_label.config(text=timeformat)
+        seconds_left -= 1
+        root.after(1000, update_timer)  # Update every 1000 ms (1 second)
+    else:
+        timer_label.config(text="Timer is up!")
 
 root = tk.Tk()
 root.title("Simple Timer")
-
 
 label = tk.Label(root, text="Enter minutes:")
 label.pack()
@@ -19,5 +29,7 @@ entry.pack()
 start_button = tk.Button(root, text="Start Timer", command=start_timer)
 start_button.pack()
 
+timer_label = tk.Label(root, text="00:00")
+timer_label.pack()
 
 root.mainloop()
